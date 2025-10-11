d5053a6c722c644a7c7b0a61963f14b34438c1c6
ValienteRitter <tagizade2006f@gmail.com>
2025-10-11 14:38:37 +0400

    Feat: Create component for editing education information

--- src/CVForm.jsx
+++ src/CVForm.jsx
@@ -1,9 +1,11 @@
 import { GeneralInfoForm } from "./components/GeneralInfoForm";
+import { EducationForm } from "./components/EducationForm";
 
 export function CVForm() {
     return (
         <>
             <GeneralInfoForm />
+            <EducationForm />
         </>
     )
 }
---
+++ src/components/EducationForm.jsx
@@ -0,0 +1,16 @@
+import { TextInput } from "./TextInput";
+
+export function EducationForm() {
+    return (
+        <section>
+            <form>
+                <TextInput label='School Name' name='school-name' id='school-name' placeholder='Azerbaijan State University of Oil and Industry' />
+                <TextInput label='Major' name='major' id='major' placeholder='Computer Science' />
+                <div>
+                    <TextInput type='date' label='From' name='from' id='from' />
+                    <TextInput type='date' label='Until' name='until' id='until' />
+                </div>
+            </form>
+        </section>
+    )
+} 
