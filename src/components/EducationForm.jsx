0ffb9b4970b0d4ec7c874b5a7c7ef305a74f39f6
ValienteRitter <tagizade2006f@gmail.com>
2025-10-11 15:06:23 +0400

    Feat: add pattern for dates to avoid unreal dates

--- src/components/EducationForm.jsx
+++ src/components/EducationForm.jsx
@@ -7,8 +7,8 @@ export function EducationForm() {
                 <TextInput label='School Name' name='school-name' id='school-name' placeholder='Azerbaijan State University of Oil and Industry' />
                 <TextInput label='Major' name='major' id='major' placeholder='Computer Science' />
                 <div>
-                    <TextInput type='date' label='From' name='from' id='from' />
-                    <TextInput type='date' label='Until' name='until' id='until' />
+                    <TextInput pattern='^(19|20)\d{2}$' label='From' name='from' id='from' placeholder='YYYY' />
+                    <TextInput pattern='^(19|20)\d{2}$' label='Until' name='until' id='until' placeholder='YYYY' />
                 </div>
             </form>
         </section>
