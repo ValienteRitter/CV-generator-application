da17f38c2ebf2d77728daccd5dcc9a9eeb17b2ea
ValienteRitter <tagizade2006f@gmail.com>
2025-10-12 15:39:11 +0400

    Refactor: Seperate dynamic input list and dynamic inputs for reusability

--- src/components/DynamicInputList.jsx
+++ src/components/DynamicInputList.jsx
@@ -1,17 +1,23 @@
 import { Input } from "./Input";
 
-export function DynamicInputList({type, handleAdd, handleDelete}) {
-    const isEducation = type === 'education' 
+export function DynamicInputList({type}) {
     return (
         <div className="dynamic-input-list">
-            <button className="add-input" onClick={handleAdd}>+</button>
+            <button className="add-input">+</button>
+            <DynamicInput type={type} />
+        </div>
+    )
+}
+
+export function DynamicInput({type}) {
+    const isEducation = type === 'education' 
+    return (
             <div className="dynamic-input">
                 <Input 
                     name={isEducation ? "learned-skill" : "job-responsibilities"}
                     placeholder={isEducation ? "Your skill" : "Your responsibility"}
                 />
-                <button className="delete-input" onClick={handleDelete}>X</button>
+                <button className="delete-input">X</button>
             </div>
-        </div>
     )
 }
