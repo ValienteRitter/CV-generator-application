87bb529c2145d354b6b0c1553366c20e1e54bd84
ValienteRitter <tagizade2006f@gmail.com>
2025-10-12 12:59:00 +0400

    Style: Style Cv Form

--- src/CVForm.jsx
+++ src/CVForm.jsx
@@ -1,6 +1,7 @@
 import { GeneralInfoForm } from "./components/GeneralInfoForm";
 import { EducationForm } from "./components/EducationForm";
 import { WorkExperienceForm } from "./components/WorkExperienceForm";
+import './styles/form.css'
 
 export function CVForm() {
     return (
