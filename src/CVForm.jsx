d62af409d89a69402c6348424330bec8c7887268
ValienteRitter <tagizade2006f@gmail.com>
2025-10-14 16:35:17 +0400

    Feat: Create button component for previewing CV

--- src/CVForm.jsx
+++ src/CVForm.jsx
@@ -1,11 +1,13 @@
 import { GeneralInfoForm } from "./components/GeneralInfoForm";
 import { EducationForm } from "./components/EducationForm";
 import { WorkExperienceForm } from "./components/WorkExperienceForm";
+import { PreviewButton } from "./components/PreviewButton";
 import './styles/form.css'
 
 export function CVForm() {
     return (
         <>
+            <PreviewButton />
             <GeneralInfoForm />
             <EducationForm />
             <WorkExperienceForm />
---
+++ src/components/PreviewButton.jsx
@@ -0,0 +1,9 @@
+export function PreviewButton() {
+    return (
+        <div className="preview-button">
+            <button>
+                &#128065;
+            </button>
+        </div>
+    )
+} 
