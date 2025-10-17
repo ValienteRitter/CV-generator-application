f8b035a833a322848c298eec28d0d8a221afa3d9
ValienteRitter <tagizade2006f@gmail.com>
2025-10-17 13:11:12 +0400

    Feat: Create state for storing data

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,60 @@
 import { useState } from "react";
-import { CVForm } from './CVForm'
+import { CVForm } from "./CVForm";
 import { CVPreview } from "./CvPreview";
 
-
 export function App() {
-    const [isPreview, setPreview] = useState(false)
+    const [isPreview, setPreview] = useState(false);
+    const [cvData, setData] = useState({
+        generalInfo: {
+            name: "",
+            email: "",
+            phone: "",
+        },
+
+        education: {
+            schoolName: "",
+            major: "",
+            skills: [],
+            from: "",
+            until: "",
+        },
+
+        work: {
+            companyName: "",
+            positionTitle: "",
+            responsibilities: [],
+            from: "",
+            until: "",
+        },
+    });
 
     function handleClick() {
-        setPreview(!isPreview)
+        setPreview(!isPreview);
     }
 
+    function handleChange(e, section, key) {
+        setData((prev) => ({
+            ...prev,
+            [section]: {
+                ...prev[section],
+                [key]: e.target.value,
+            },
+        }));
+        console.log(cvData)
+    }
 
     return (
         <>
-            {isPreview ? <CVPreview handleClick={handleClick} /> : <CVForm handleClick={setPreview}/>}
+            {isPreview ? (
+                <CVPreview data={cvData} handleClick={handleClick} />
+            ) : (
+                <CVForm
+                    handleClick={setPreview}
+                    handleChange={(e, section,key) => {
+                        handleChange(e, section, key);
+                    }}
+                />
+            )}
         </>
     );
 }
-
